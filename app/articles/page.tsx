ad81ba8807219f0cde6e5b0b38f64557e4cab13f
Jack Skerman <jack.skerm@gmail.com>
2024-03-06 11:07:46 +0100

    remove article grid

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -5,13 +5,14 @@ import { Article } from "@/api/types";
 import { colors } from "@/theme";
 import Image from "next/image";
 import Link from "next/link";
+import Card from "@/components/ui/card";
 
 const Articles = async () => {
   const articles = await readArticles();
   return (
     <Flex flexDir={"column"} gap={4}>
       <Heading>Articles</Heading>
-      <SimpleGrid minChildWidth={{ base: "250px", md: "350px" }} spacing="20px">
+      <Flex gap={4} flexDir={"column"}>
         {articles.data.map((content) => (
           <ArticleCard
             key={content.id}
@@ -19,49 +20,47 @@ const Articles = async () => {
             article={content.attributes}
           />
         ))}
-      </SimpleGrid>
+      </Flex>
     </Flex>
   );
 };
 
 const ArticleCard = ({ id, article }: { id: number; article: Article }) => (
   <Link href={`/articles/${id}`} className="h-full">
-    <Flex
-      className="transition hover:scale-[102%] hover:bg-hollywood-500"
-      borderColor={"black"}
-      borderWidth={2}
-      textColor={"white"}
-      flexDir={"column"}
-      p={4}
-      gap={4}
-      bg={colors["pink-salmon"][400]}
-      shadow={"5px 5px 0 black"}
-      h={"full"}
-      justifyContent={"space-between"}
-    >
-      <Flex flexDir={"column"}>
-        <Heading fontSize={"xl"}>{article.title}</Heading>
-        <Text>{article.description}</Text>
-      </Flex>
-      <Box minH={150} w="full">
-        <Image
-          src="https://picsum.photos/300/200"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-          alt="Article Thumbnail"
-        />
-      </Box>
+    <Card>
       <Flex
+        textColor={"white"}
         flexDir={"row"}
+        gap={4}
         justifyContent={"space-between"}
-        alignItems={"center"}
       >
-        <Avatar src="https://bit.ly/ryan-florence" size={"sm"} />
-        <Text>{new Date(article.publishedAt).toDateString()}</Text>
+        <Flex w={"60%"} flexDir={"column"} justifyContent={"space-between"}>
+          <Box>
+            <Heading fontSize={"xl"}>{article.title}</Heading>
+            <Text color={"black"}>{article.description}</Text>
+          </Box>
+          <Flex
+            gap={2}
+            flexDir={"row"}
+            justifyContent={"start"}
+            alignItems={"center"}
+          >
+            <Avatar src="https://bit.ly/ryan-florence" size={"sm"} />
+            <Text>{new Date(article.publishedAt).toDateString()}</Text>
+          </Flex>
+        </Flex>
+        <Box w={"40%"}>
+          <Image
+            src="https://picsum.photos/300/200"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+            alt="Article Thumbnail"
+          />
+        </Box>
       </Flex>
-    </Flex>
+    </Card>
   </Link>
 );
 
