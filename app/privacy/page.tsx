dd6cd700b3f65d0b3c048136ed1fce2a5fac9fc2
Jack Skerman <jack.skerm@gmail.com>
2024-03-13 23:11:47 +0100

    termly-script-wrapper component

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,29 +1,9 @@
-import Script from "next/script";
-import React from "react";
+import TermlyScriptWrapper from './termly-script-wrapper';
 
-const html = `<div name="termly-embed" data-id="b0c0190e-5928-4b28-b76c-8f568fde2d68"></div>
-<script type="text/javascript"></script>`;
 
-const divProps = {
-  name: "termly-embed",
-  "data-id": "b0c0190e-5928-4b28-b76c-8f568fde2d68",
-};
 
 const Privacy = () => {
-  return (
-    <div>
-      <div {...divProps} />
-      <Script type="text/javascript">
-        {`(function(d, s, id) {
-          var js, tjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "https://app.termly.io/embed-policy.min.js";
-          tjs.parentNode.insertBefore(js, tjs);
-        }(document, 'script', 'termly-jssdk'));`}
-      </Script>
-    </div>
-  );
+  return <TermlyScriptWrapper dataId="b0c0190e-5928-4b28-b76c-8f568fde2d68"/>
 };
 
 export default Privacy;
