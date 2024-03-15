6dbd6446cbe2a0e29d25445865e6b01ebc876684
ngd adams <ngd.adams@gmail.com>
2024-03-15 16:12:12 +0100

    gold article URL
    Fixed spelling mistake in gold URL article

--- static/investments.ts
+++ static/investments.ts
@@ -4,18 +4,21 @@ const investments = [
     name: "S&P500 Index ETF",
     description:
       "Seeks to track an index composed of the 500 largest U.S. corporations",
+      rvol: "6.6",
     link: "https://www.investopedia.com/articles/investing/090414/sp-500-etfs-what-every-investor-should-know.asp",
   },
   {
     id: "GOVT",
     name: "Government Bond ETF",
     description: "Seeks to track an index composed of U.S. Treasury bonds",
+    rvol: "4.5",
     link: "https://www.ig.com/en/bonds/what-are-government-bonds",
   },
   {
     id: "JNJ",
     name: "Johnson & Johnson",
     description: "An Income Equity",
+    rvol: "7.5",
     link: "https://corporatefinanceinstitute.com/resources/career-map/sell-side/capital-markets/income-stocks/",
   },
   {
@@ -23,12 +26,14 @@ const investments = [
     name: "Emerging Markets ETF",
     description:
       "Seeks to track an index composed of large- and mid-sized emerging market equities",
+      rvol: "7.4",
     link: "https://www.investopedia.com/terms/e/emerging-market-etf.asp",
   },
   {
     id: "AMZN",
     name: "Amazon",
     description: "A Growth Stock",
+    rvol: "9.8",
     link: "https://corporatefinanceinstitute.com/resources/career-map/sell-side/capital-markets/growth-stocks/",
   },
   {
@@ -36,25 +41,22 @@ const investments = [
     name: "REIT ETF",
     description:
       "Seeks to track an index composed of U.S. real estate equities",
+      rvol: "8.2",
     link: "https://www.investor.gov/introduction-investing/investing-basics/investment-products/real-estate-investment-trusts-reits",
-  },
-  {
-    id: "IBIT",
-    name: "Bitcoin ETF",
-    description: "Seeks to reflect the performance of the price of bitcoin",
-    link: "https://medium.com/free-code-camp/explain-bitcoin-like-im-five-73b4257ac833",
   },
   {
     id: "VTC",
     name: "Corporate Bond ETF",
     description: "Seeks to track an index composed of U.S. Corporate bonds",
+    rvol: "5.0",
     link: "https://www.investor.gov/introduction-investing/investing-basics/investment-products/bonds-or-fixed-income-products",
   },
   {
     id: "GLD",
     name: "Gold ETF",
     description: "Seeks to track the price of gold bullion in the OTC market",
-    link: "[https://www.investopedia.com/articles/investing/122515/gld-ishares-gold-trust-etf.asp#:~:text=GLD tracks the price of,tracking error of around 0.93%25](https://www.investopedia.com/articles/investing/122515/gld-ishares-gold-trust-etf.asp#:~:text=GLD%20tracks%20the%20price%20of,tracking%20error%20of%20around%200.93%25).",
+    rvol: "6.7",
+    link: "https://www.investopedia.com/articles/investing/122515/gld-ishares-gold-trust-etf.asp#:~:text=GLD tracks the price of,tracking error of around 0.93%25](https://www.investopedia.com/articles/investing/122515/gld-ishares-gold-trust-etf.asp#:~:text=GLD%20tracks%20the%20price%20of,tracking%20error%20of%20around%200.93%25).",
   },
 ];
 
